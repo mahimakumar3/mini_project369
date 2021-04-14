d0fb616dd8fb82d6d60c7469caacf6c877587424
mahimakumar3 <80737223+mahimakumar3@users.noreply.github.com>
2021-04-15 01:47:54 +0530

    Add files via upload

---
+++ Implementation/inc/calculator.h
@@ -0,0 +1,13 @@
+#ifndef _calculator_h_
+#define _calculator_h_
+
+// Function prototype declaration
+void addition();
+void subtraction();
+void multiplication();
+void division();
+void power();
+int factorial();
+void calculator_operations();
+
+#endif /* _shit.h_ */
